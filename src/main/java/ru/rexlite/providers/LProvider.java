c288845e1289b6ff93a7fde56bb175c88557006b
Ivan <sunblog0bg@gmail.com>
2025-04-14 20:49:53 +0300

    Fixed prefix and suffix autoupdate

--- src/main/java/ru/rexlite/providers/LProvider.java
+++ src/main/java/ru/rexlite/providers/LProvider.java
@@ -7,9 +7,14 @@ import net.luckperms.api.model.user.User;
 
 public class LProvider implements PrefixSuffixProvider {
 
+    private final LuckPerms luckPerms;
+
+    public LProvider() {
+        this.luckPerms = LuckPermsProvider.get();
+    }
+
     @Override
     public String getPrefix(Player player) {
-        LuckPerms luckPerms = LuckPermsProvider.get();
         User user = luckPerms.getUserManager().getUser(player.getUniqueId());
         return user != null && user.getCachedData().getMetaData().getPrefix() != null
                 ? user.getCachedData().getMetaData().getPrefix()
@@ -18,10 +23,13 @@ public class LProvider implements PrefixSuffixProvider {
 
     @Override
     public String getSuffix(Player player) {
-        LuckPerms luckPerms = LuckPermsProvider.get();
         User user = luckPerms.getUserManager().getUser(player.getUniqueId());
         return user != null && user.getCachedData().getMetaData().getSuffix() != null
                 ? user.getCachedData().getMetaData().getSuffix()
                 : "";
+    }
+
+    public LuckPerms getLuckPerms() {
+        return luckPerms;
     }
 }
